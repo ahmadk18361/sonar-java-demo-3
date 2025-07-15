032555d7769e2ddee128ef2a638857290d2df950
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-15 13:16:18 -0400

    Update Log4jBufferedInputExample.java

--- src/main/java/com/example/Log4jBufferedInputExample.java
+++ src/main/java/com/example/Log4jBufferedInputExample.java
@@ -6,10 +6,9 @@ public class Log4jBufferedInputExample {
     static Logger logger = Logger.getLogger(Log4jBufferedInputExample.class);
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
         System.out.print("Enter username: ");
-        String username = reader.readLine();  //  User-controlled input
-        logger.warn("User login failed: " + String.valueOf(username));  //  Dangerous logging
+        String username = reader.readLine();
+        logger.warn("User login failed: " + String.valueOf(username));
     }
 }
-
